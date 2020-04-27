8a345dc356a6f25a1bfe37283ccfc2562754e5c2
mghol1 <64386178+mghol1@users.noreply.github.com>
2020-04-27 10:48:48 +0200

    Add files via upload

---
+++ MGHOL*-+=.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo " _______________"
+echo '               /'
+echo "              /"
+echo '             /'
+echo '            /'
+echo '           /'
+echo '          /'
+echo '         /'
+echo '        /'
+echo '       /______________'
+read -p" <<<===========ENTER===========>>> "
+read -p "enter number : " a
+echo "= "$(( "$a" ))
