c993903fda2daac5b23fd8eacd709fe6e908b623
mghol1 <64386178+mghol1@users.noreply.github.com>
2020-04-27 11:21:05 +0200

    Update MGHOL*-+=.sh

--- MGHOL*-+=.sh
+++ MGHOL*-+=.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo " _______________"
+printf "\e[1;92m ___________________"                                                   echo "                    /"
+echo "                   /"
+echo '                  /'
+echo '                 /'
+echo "                /"
 echo '               /'
-echo "              /"
+echo '              /'
 echo '             /'
-echo '            /'
-echo '           /'
-echo '          /'
-echo '         /'
-echo '        /'
-echo '       /______________'
+printf "\e[1;92m            /______________\e[0m"
+echo
 read -p" <<<===========ENTER===========>>> "
 read -p "enter number : " a
 echo "= "$(( "$a" ))
