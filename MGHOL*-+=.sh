3f0d1b999af903b3bf5313f0912cbd371c0c8f35
mghol1 <64386178+mghol1@users.noreply.github.com>
2020-04-28 02:13:18 +0200

    Delete MGHOL*-+=.sh

--- MGHOL*-+=.sh
+++
@@ -1,4 +0,0 @@
-GNU nano 4.6                          MGHOL*_+=.sh
-z="
-";Oz=''\''';Tz='e[0m';Mz='/'\''';az='===>';dz='"ent';Bz='tf "';Dz=';92m';Hz='echo';Zz='>
-eval "$Az$Bz$Cz$Dz$Ez$Fz$Fz$Fz$Fz$Gz$z$Hz$Iz$Jz$Jz$Jz$Jz$Kz$z$Hz$Lz$Jz$Jz$Jz$Jz$Mz$z$Hz>
